145bc871c2418363987157ac4589def7b111c135
Chuanqi Xu <466018122@qq.com>
2022-12-18 09:43:39 -0500

    finished code 20

---
+++ computer-science/Stanford-CS106X-Programming-Abstractions-in-CPP-2019-Fall/CodeStepByStep/20 binary search trees (BSTs)/bstAdd1.cpp
@@ -0,0 +1,3 @@
+// pre-order: Dodo, Cat, Alice, Bill, Eaglet, Rabbit, Jabberwock, Hatter, Queen, Tweedledee
+// in-order: Alice, Bill, Cat, Dodo, Eaglet, Hatter, Jabberwock, Queen, Rabbit, Tweedledee
+// post-order: Bill, Alice, Cat, Hatter, Queen, Jabberwock, Tweedledee, Rabbit, Eaglet, Dodo
---
+++ computer-science/Stanford-CS106X-Programming-Abstractions-in-CPP-2019-Fall/CodeStepByStep/20 binary search trees (BSTs)/bstAdd3.cpp
@@ -0,0 +1,5 @@
+/* binary search tree: b
+* pre-order: Leia, Boba, Anakin, Darth, Chewy, Han, Jabba, R2D2, Luke, Palpatine
+* in-order: Anakin, Boba, Chewy, Darth, Han, Jabba, Leia, Luke, Palpatine, R2D2
+* post-order: Anakin, Chewy, Jabba, Han, Darth, Boba, Palpatine, Luke, R2D2, Leia
+*/
