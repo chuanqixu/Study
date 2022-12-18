1ce60b19bafa4e9de9a85e1c782a27cfe5cd2996
Chuanqi Xu <466018122@qq.com>
2022-12-18 09:53:40 -0500

    finished code 22

---
+++ computer-science/Stanford-CS106X-Programming-Abstractions-in-CPP-2019-Fall/CodeStepByStep/22 graphs; DFS, BFS/graphProperties1.cpp
@@ -0,0 +1,14 @@
+/* Is the graph directed or undirected? directed
+* Is the graph weighted or unweighted? unweighted
+* Is the graph connected? not connected
+* Is the graph cyclic? cyclic
+* What are the in-degree and out-degree of vertex A (as two integers, separated by spaces)? 0 2
+* What are the in-degree and out-degree of vertex B? 2 1
+* What are the in-degree and out-degree of vertex C? 1 1
+* What are the in-degree and out-degree of vertex D? 2 1
+* What are the in-degree and out-degree of vertex E? 2 2
+* What are the in-degree and out-degree of vertex F? 2 1
+* What are the in-degree and out-degree of vertex G? 2 1
+* What are the in-degree and out-degree of vertex H? 2 2
+* What are the in-degree and out-degree of vertex I? 0 2
+*/
