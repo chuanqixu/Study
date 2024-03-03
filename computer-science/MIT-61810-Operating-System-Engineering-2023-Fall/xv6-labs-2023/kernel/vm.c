228d30a28257840e4735b7a57df81d0d940b94fb
Chuanqi Xu <466018122@qq.com>
2024-03-03 12:06:57 -0500

    mit-6.1810/lab page tables: print a page table

--- computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/kernel/vm.c
+++ computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/kernel/vm.c
@@ -449,3 +449,22 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable, int level) {
+  char *indent = " ..";
+  if (level == 2) {
+    printf("page table %p\n", pagetable);
+  }
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for (int j = 0; j < 3 - level; ++j) {
+        printf("%s", indent);
+      }
+      uint64 pa = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+      if (level > 0)
+        vmprint((pagetable_t) pa, level - 1);
+    }
+  }
+}
