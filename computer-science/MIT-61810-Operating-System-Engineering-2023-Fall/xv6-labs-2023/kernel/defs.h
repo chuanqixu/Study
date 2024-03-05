d21c78a5cbc7a6f9b2f41190a1fd9164f3748d4b
Chuanqi Xu <466018122@qq.com>
2024-03-05 12:59:10 -0500

    mit-6.1810/lab traps: init

--- computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/kernel/defs.h
+++ computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/kernel/defs.h
@@ -173,8 +173,6 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-void            vmprint(pagetable_t, int);
-int             uvmpgaccess(pagetable_t, void *, int, void *);
 
 // plic.c
 void            plicinit(void);
