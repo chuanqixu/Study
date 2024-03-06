4fad22ca05de73899ca4cfb388f58bdc9182e93e
Chuanqi Xu <466018122@qq.com>
2024-03-05 20:56:43 -0500

    mit-6.1810/lab traps: risc-v  assembly

--- computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/user/call.c
+++ computer-science/MIT-61810-Operating-System-Engineering-2023-Fall/xv6-labs-2023/user/call.c
@@ -13,5 +13,21 @@ int f(int x) {
 
 void main(void) {
   printf("%d %d\n", f(8)+1, 13);
+
+  // registers are stored in a0, a1, and a2
+
+  // call to f and g is inlined, i.e., there is no call and values are added directly
+
+  // printf is located at the place in the comment in call.asm
+  
+  // jalr will store the next instruction address to ra
+
+  // unsigned int i = 0x00646c72;
+	// printf("H%x Wo%s", 57616, &i); // HE110 World
+  // 57616 = 0xE110, 72 -> r, 6c -> l, 62 -> d, 00 -> \n
+
+  // printf("x=%d y=%d", 3); // x=3 y=8229
+  // the second %d will use the values in a2, which is the garbage value
+
   exit(0);
 }
