4637121ae9bdedaa5dcdba1e21a509158181ff5f
Chuanqi Xu <466018122@qq.com>
2024-04-07 19:55:17 -0400

    stanford-cs144/lab 0: 3 writing a network program using an OS stream socket

--- computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/apps/webget.cc
+++ computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/apps/webget.cc
@@ -9,8 +9,30 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+  TCPSocket tcp_socket;
+  tcp_socket.connect(Address(host, "http"));
+
+  string cmd;
+  cmd += string("GET ") + path + string(" HTTP/1.1\r\n");
+  cmd += string("Host: ") + host + string("\r\n");
+  cmd += string("Connection: close\r\n");
+  cmd += string("\r\n");
+
+  tcp_socket.write(cmd);
+  tcp_socket.shutdown(SHUT_WR);
+
+  string recv_str, curr_str;
+  while (!tcp_socket.eof()) {
+    tcp_socket.read(curr_str);
+    recv_str += curr_str;
+  }
+
+  tcp_socket.close();
+
+  cout << recv_str;
 }
 
 int main( int argc, char* argv[] )
