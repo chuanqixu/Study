8697b49d92a20ed8662fd6d6ebabdc85652bb3fc
Chuanqi Xu <466018122@qq.com>
2024-04-21 17:20:01 -0400

    stanford-cs144/lab 2: tcp_receiver

--- computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/tcp_receiver.cc
+++ computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/tcp_receiver.cc
@@ -5,11 +5,35 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  (void)message;
+  uint64_t abs_seqno;
+  if ( message.RST ) {
+    reader().set_error();
+    return;
+  } else if ( !message.SYN ) {
+    if ( isn_.has_value() ) // have connected before
+      abs_seqno = message.seqno.unwrap( isn_.value(), writer().bytes_pushed() );
+    else // have not connected yet
+      return;
+  } else { // SYN is set
+    isn_ = Wrap32( message.seqno + 1 );
+    abs_seqno = 0;
+  }
+
+  reassembler_.insert( abs_seqno, std::move( message.payload ), message.FIN );
+
+  // FIN may be sent before all strings can be reassembled
+  // in this case, the message may be out of order, and in the following previous bytes may be receive
+  // so the condition is to check whether writer is finished (writer is finished when having received FIN and
+  // reassembling all strings)
+  if ( writer().is_closed() )
+    isn_ = isn_.value() + 1;
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  return {};
+  const Writer& w = writer();
+  return { isn_.has_value() ? Wrap32::wrap( w.bytes_pushed(), isn_.value() ) : isn_,
+           static_cast<uint16_t>( w.available_capacity() > UINT16_MAX ? UINT16_MAX : w.available_capacity() ),
+           w.has_error() };
 }
--- computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/wrapping_integers.cc
+++ computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/wrapping_integers.cc
@@ -12,14 +12,15 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
   const uint64_t checkpoint_higher32 = checkpoint & 0xffffffff00000000;
-  const uint64_t sqeno = (raw_value_ - zero_point.raw_value_) | checkpoint_higher32;
+  const uint64_t sqeno = ( raw_value_ - zero_point.raw_value_ ) | checkpoint_higher32;
   uint64_t half = 1ULL << 31;
 
-  if (checkpoint > sqeno && checkpoint - sqeno > half) {
+  if ( checkpoint > sqeno && checkpoint - sqeno > half ) {
     half <<= 1;
     return sqeno + half;
-  } else if (checkpoint < sqeno && sqeno - checkpoint > half){
-    if (checkpoint_higher32 == 0) return sqeno;
+  } else if ( checkpoint < sqeno && sqeno - checkpoint > half ) {
+    if ( checkpoint_higher32 == 0 )
+      return sqeno;
     half <<= 1;
     return sqeno - half;
   }
