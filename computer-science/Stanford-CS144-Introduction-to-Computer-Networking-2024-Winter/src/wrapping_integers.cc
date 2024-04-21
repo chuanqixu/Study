e22e704dfd7f09de56625dd1152df724e512a81f
Chuanqi Xu <466018122@qq.com>
2024-04-20 20:20:59 -0400

    stanford-cs144/lab 2: wrap32

--- computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/wrapping_integers.cc
+++ computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/wrapping_integers.cc
@@ -5,15 +5,24 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  const uint64_t checkpoint_higher32 = checkpoint & 0xffffffff00000000;
+  const uint64_t sqeno = (raw_value_ - zero_point.raw_value_) | checkpoint_higher32;
+  uint64_t half = 1ULL << 31;
+
+  if (checkpoint > sqeno && checkpoint - sqeno > half) {
+    half <<= 1;
+    return sqeno + half;
+  } else if (checkpoint < sqeno && sqeno - checkpoint > half){
+    if (checkpoint_higher32 == 0) return sqeno;
+    half <<= 1;
+    return sqeno - half;
+  }
+
+  return sqeno;
 }
