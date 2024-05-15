4c5fdff23ac73a67e47e891de4cd9ad2cc0fb7d3
Chuanqi Xu <466018122@qq.com>
2024-05-15 08:30:10 -0400

    stanford-cs144/lab 6: finished

--- computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/router.cc
+++ computer-science/Stanford-CS144-Introduction-to-Computer-Networking-2024-Winter/src/router.cc
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <limits>
+#include <queue>
 
 using namespace std;
 
@@ -21,10 +22,29 @@ void Router::add_route( const uint32_t route_prefix,
        << " on interface " << interface_num << "\n";
 
   // Your code here.
+  routes_.add( route_prefix, prefix_length, next_hop, interface_num );
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
   // Your code here.
+  for ( auto& interface : _interfaces ) {
+    queue<InternetDatagram>& q = interface->datagrams_received();
+    while ( !q.empty() ) {
+      InternetDatagram& dgram = q.front();
+      uint32_t ip_num = dgram.header.dst;
+      pair<optional<Address>, size_t> action;
+      if ( dgram.header.ttl > 1 && routes_.find( ip_num, action ) ) {
+        --dgram.header.ttl;
+        dgram.header.compute_checksum();
+        if ( action.first.has_value() ) {
+          _interfaces[action.second]->send_datagram( dgram, action.first.value() );
+        } else {
+          _interfaces[action.second]->send_datagram( dgram, Address::from_ipv4_numeric( ip_num ) );
+        }
+      }
+      q.pop();
+    }
+  }
 }
